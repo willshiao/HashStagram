571d7a2bc4c53ceb5868cd522c20eefeb4998df2
jjkjiang <jerryjiekaijiang@gmail.com>
2018-10-13 02:26:12 -0700

    Add mechanism_type envvar
    To switch between different architecture modes
    to test tradeoffs, we will have code for both
    types available and use an environmental variable
    to denote which mode to switch to.
    
    PubSub is more expensive to scale for the proposed
    ecosystem we brainstormed, while the storage system
    is significantly cheaper.

--- cloud-functions/backend/main.py
+++ cloud-functions/backend/main.py
@@ -12,13 +12,19 @@ def upload(request):
         `make_response <http://flask.pocoo.org/docs/0.12/api/#flask.Flask.make_response>`.
     """
     request_json = request.get_json()
-    if request_json and 'photo' in request_json:
-        publisher = pubsub.PublisherClient()
-        topic_name = 'projects/{project_id}/topics/{topic}'.format(
-            project_id=os.getenv('GOOGLE_CLOUD_PROJECT'),
-            topic=os.getenv('topic_name'),  # Set this to something appropriate.
-        )
-        publisher.publish(topic_name, b'Test', photo=request_json['photo'])
-        return Response({topic_name + " published"}, status=200);
+
+		if os.getenv('mechanism_type') == 'pubsub':
+        if request_json and 'photo' in request_json:
+            publisher = pubsub.PublisherClient()
+            topic_name = 'projects/{project_id}/topics/{topic}'.format(
+                project_id=os.getenv('GOOGLE_CLOUD_PROJECT'),
+                topic=os.getenv('topic_name'),  # Set this to something appropriate.
+            )
+            publisher.publish(topic_name, b'Test', photo=request_json['photo'])
+            return Response({topic_name + " published"}, status=200);
+        else:
+            return Response({"Missing photo argument"},status=400)
+    elif os.getenv('mechanism_type') == 'storage':
+        return Response({"TBA"}, status=403)
     else:
-        return Response({"Missing photo argument"},status=400)
+        return Response({"Misconfiguration error, contact administrator"}, status=500)
