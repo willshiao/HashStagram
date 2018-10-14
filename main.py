666faf87a775d6ffd84fa9f1bf7ddfbd0963a8c1
Jerry Jiang <jjian014@ucr.edu>
2018-10-14 03:16:45 +0000

    Add change for easier static site placement

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pickle
 import io
 import numpy
 from PIL import Image
+import logging
 
 from google.cloud import pubsub, storage
 
@@ -21,11 +22,7 @@ from wideresnet import *
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods = ["GET"])
-def index():
-    return render_template('index.html')
-
-@app.route("/upload", methods = ["POST"])
+@app.route("/api/upload", methods = ["POST"])
 def upload():
     """
         Takes in a photo and attempts to serve to the user hashtags.
@@ -35,14 +32,19 @@ def upload():
     if request.files and 'photo' in request.files:
         photo = request.files['photo']
         img = Image.open(photo)
+        app.logger.error(img)
         width, height = img.size
+        app.logger.error(str(width) + " " +  str(height))
         data = numpy.asarray(img, dtype=numpy.float32)
-        data = numpy.reshape(data, (3, width, height))
-        tensor = torch.from_numpy(data).unsqueeze(0)
+        app.logger.error(data)
+        #data = numpy.reshape(data, (3, width, height))
+        tensor = torch.from_numpy(data).unsqueeze_(0)
+        app.logger.error(tensor)
         model = app.config['MODEL']
+        app.logger.error(model)
         #return Response({str(tensor.size())}, status=200)
         #tensor = torch.rand(1,3,224,224)
         result = model.forward(tensor)
         return Response({"forwarded successfully"}, status=200)
-    else:
+    else:           
         return Response({"Missing photo argument"}, status=400)
