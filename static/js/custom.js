d6b7492390a5afbb7286c62c40c6abe341f76d1e
William Shiao <willshiao@gmail.com>
2018-10-14 08:48:30 -0700

    Improve site error handling

--- static/js/custom.js
+++ static/js/custom.js
@@ -4,75 +4,82 @@
 // orientation) and size validation, to prevent
 // large files from being added
 FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageExifOrientation
-    // FilePondPluginFileValidateType
-    // FilePondPluginImageValidateSize,
-    // FilePondPluginFileValidateSize
+  FilePondPluginImagePreview,
+  FilePondPluginImageExifOrientation,
+  FilePondPluginFileValidateType,
+  FilePondPluginImageValidateSize,
+  FilePondPluginFileValidateSize
 );
 
 // multiple pngs (up to 3) accepted without spitting out error message
 FilePond.setOptions({
-    // allowBrowse: false,
-    acceptedFileTypes: ['image/png', 'image/jpeg', 'image/jpg', 'image/bmp']
+  // allowBrowse: false,
+  acceptedFileTypes: ['image/png', 'image/jpeg', 'image/jpg', 'image/bmp']
 });
 
 // Select the file input and use 
 // create() to turn it into a pond
 const trueOptions = {
-    maxFiles: 1
+  maxFiles: 1
 }
 const pond = FilePond.create(document.querySelector('input.filepond'))
 Object.assign(pond, trueOptions)
 
 function postData(url = ``, file) {
-    // Default options are marked with *
-    const formData = new FormData()
+  // Default options are marked with *
+  const formData = new FormData()
 
-    formData.append("photo", file)
+  formData.append('photo', file)
 
-    return fetch(url, {
-        method: "POST",
-        body: formData
+  return fetch(url, {
+    method: 'POST',
+    body: formData
+  })
+    .then((response) => {
+      console.log(response)
+      return Promise.resolve(response.json())
+      // now put response on the picture (see below, line 51 or so)
     })
-        .then((response) => {
-            console.log(response)
-            return Promise.resolve(response.json())
-            // now put response on the picture (see below, line 51 or so)
-        })
+    .catch(err => {
+      console.error(err)
+    })
 }
 
-
 $(function () {
-    let formDisabled = false
+  let formDisabled = false
 
-    $('#upload-btn').click(function (evt) {
-        evt.preventDefault()
-        if (formDisabled) return false
+  $('#upload-btn').click(function (evt) {
+    evt.preventDefault()
+    if (formDisabled) return false
 
-        console.log('Button pressed!')
-        formDisabled = true
-        $('#upload-btn').addClass('disabled')
-        const f = pond.getFile().file
+    console.log('Button pressed!')
+    formDisabled = true
+    $('#upload-btn').addClass('disabled')
+    const f = pond.getFile().file
 
+    postData('/api/upload', f)
+      .then(res => {
+        if (res !== 200) {
+          $('#img-caption').text('An error occurred: ', res.statusText)
+          formDisabled = false
+          $('#upload-btn').removeClass('disabled')
+          return
+        }
+        console.log('Got response from Jerry :)', res)
+        const reader = new FileReader()
+        reader.readAsDataURL(f)
 
-        postData('/api/upload', f)
-            .then(res => {
-                console.log('Got response from Jerry :)', res)
-                const reader = new FileReader()
-                reader.readAsDataURL(f)
+        reader.onload = function (evt) {
+          if (evt.target.readyState == FileReader.DONE) {
+            $('#dispupload').html(`<img src="${evt.target.result}" class="uploaded-img">`)
+            $('#img-caption').text(res.data.join(', '))
+            formDisabled = false
+            $('#upload-btn').removeClass('disabled')
+          }
+        }
+      })
 
-                reader.onload = function (evt) {
-                    if (evt.target.readyState == FileReader.DONE) {
-                        $('#dispupload').html(`<img src="${evt.target.result}" class="uploaded-img">`)
-                        $('#img-caption').text(res.data.join(', '))
-                        formDisabled = false
-                        $('#upload-btn').removeClass('disabled')
-                    }
-                }
-            })
-
-    })
+  })
 })
 
 // postData(`https://us-central1-sdhacksproject2018.cloudfunctions.net/dummy`, {answer: 42})
