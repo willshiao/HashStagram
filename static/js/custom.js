63ed8987168ce1938ec6300c3a2511afe79913c6
William Shiao <willshiao@gmail.com>
2018-10-14 09:55:11 -0700

    Use a bootstrap card to display results

--- static/js/custom.js
+++ static/js/custom.js
@@ -74,11 +74,16 @@ $(function () {
 
             reader.onload = function (evt) {
               if (evt.target.readyState == FileReader.DONE) {
-                $('#dispupload').html(`<img src="${evt.target.result}" class="uploaded-img">`)
                 const hashtagText = resData.data
                   .map(tag => `<a href="https://www.instagram.com/explore/tags/${tag}/">#${tag}</a>`)
                   .join(' ')
-                $('#img-caption').text(hashtagText)
+
+                $('#image-info').html(`
+                <img class="card-img-top" src="${evt.target.result}" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text">${hashtagText}</p>
+                </div>
+                `)
 
                 formDisabled = false
                 $('#upload-btn').removeClass('disabled')
